b950e8be42ef49233221dee9d9bd8632cac18e4f
alyaaomar <87870799+lolaomar@users.noreply.github.com>
2021-11-13 15:22:19 +0200

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,1 +1,68 @@
 <?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Compound Help Desk System - Signup</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<form class="login100-form validate-form p-l-55 p-r-55 p-t-178" method="POST" action="controller/signup.php">
+					<span class="login100-form-title">
+						Sign Up
+					</span>
+
+						<input class="input100" type="text" name="DisplayName" placeholder="DisplayName">
+						<span class="focus-input100"></span>
+					<br/>
+						<input class="input100" type="password" name="pass" placeholder="Password">
+						<span class="focus-input100"></span>
+					<br/>
+
+						<input class="input100" type="password" name="pass" placeholder="ConfirmPassword">
+						<span class="focus-input100"></span>
+					<br/>
+
+						<input class="input100" type="email" name="pass" placeholder="E_mail">
+						<span class="focus-input100"></span>
+					<br/>
+
+						<button class="login100-form-btn">
+							Sign Up
+						</button>
+					<br/>
+					
+						<a href="index.php" class="txt3">
+							Log In now
+						</a>
+
+				</form>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
