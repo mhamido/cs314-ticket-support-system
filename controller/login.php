4557b62f277a0a2182764bed04da77d731aa48a9
MiltonHs <mahmoudmhamido@gmail.com>
2021-11-13 21:49:11 +0200

    amended login controller

--- controller/login.php
+++ controller/login.php
@@ -17,6 +17,20 @@ if (isNullOrEmpty($password)) {
 
 if (empty($errs)) {
     echo "Attempted to login with $email and $password";
+    $stmt = DatabaseConnection::getInstance()->prepare(
+        "SELECT user.id FROM user
+            WHERE user.email=? AND user.Password=?"
+    );
+    $stmt->bind_param('ss', $email, $password);
+    $result = $stmt->execute();
+
+    if ($result) {
+        $result = $stmt->get_result();
+        if ($result->num_rows === 1) {
+            $id = $result->fetch_assoc()["id"];
+            $usr = new User($id);
+        }
+    }
 } else {
     foreach ($errs as $err) {
         echo "Error: $err <br>";
