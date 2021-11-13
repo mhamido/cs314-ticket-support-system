c71d1531ea99784fc169d72d61107681ff1362d2
Bloz <93732839+OmarBloz@users.noreply.github.com>
2021-11-13 14:40:16 +0200

    Update user.php

--- model/user.php
+++ model/user.php
@@ -1,5 +1,5 @@
 <?php
-require_once "database.php";
+
 class User
 {
     private $id;
@@ -11,38 +11,37 @@ class User
 
     public function __construct($id)
     {
-       $stmt = "SELECT * FROM user WHERE user.id=$id";
-       $result= DatabaseConnection::getInstance()->query($stmt);
-        if(!$result){
-            echo("User not found");
-                return;
+        $stmt = "SELECT * FROM user WHERE user.id=$id";
+        $result = DatabaseConnection::getInstance()->query($stmt);
+
+        if (!$result) {
+            echo "User with $id not found.";
+            return;
         }
-        
-        $this->id=$id;
-        $this->displayName= $result["DisplayName"];
-        $this->email=$result["email"];
-        $this->password=$result["Password"];
-        $this->lastLogin=$result["LastLogin"];
-        $this->signupDate=$result["SignupDate"];
-        
+
+        $this->id = $id;
+        $this->displayName = $result["DisplayName"];
+        $this->email = $result["email"];
+        $this->password = $result["Password"];
+        $this->lastLogin = $result["LastLogin"];
+        $this->signupDate = $result["SignupDate"];
     }
 
-
-    public static function login($email,$password)
+    public static function login($email, $password)
     {
-        $stmt= "SELECT user.id WHERE user.email=$email AND user.Password=$password";
+        $stmt = "SELECT user.id WHERE user.email=$email AND user.Password=$password";
         $result = DatabaseConnection::getInstance()->query($stmt);
-        if(!$result){
-            echo 'Invalid Email/Password.';
+
+        if (!$result) {
+            echo 'Invalid email/password.';
             return;
-            
         }
-        $id=$result["id"];
+        
+        $id = $result["id"];
         return new User($id);
     }
 
     public static function signup()
     {
-        
     }
 }
