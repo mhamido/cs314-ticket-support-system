cbc2f870c47fb84397d4853ca6b4c7419bc5efac
maryam <93222257+maryamusama@users.noreply.github.com>
2021-11-15 18:10:55 +0200

    Update landscaping.php

--- model/landscaping.php
+++ model/landscaping.php
@@ -1,1 +1,20 @@
+<?php
+require_once 'service.php';
+class LandScaping implements Service{
+    
+    public function price()
+    {
+        return;
+    }
+    public function perform()
+    {
+      return;
+    }
+    public function description()
+    {
+     return;
+    }
 
+}
+
+?>
