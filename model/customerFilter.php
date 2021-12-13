32bb6c727191869381455ff3d6005b985c9f8fdc
maryam <93222257+maryamusama@users.noreply.github.com>
2021-12-13 22:54:20 +0200

    Update customerFilter.php

--- model/customerFilter.php
+++ model/customerFilter.php
@@ -3,14 +3,14 @@ require_once 'filter.php';
 class CustomerFilter extends Filter
 {
     public $user;
-    public function __construct()
+    public function __construct($user)
     {
+        $this->user = $user;
     }
     public function generate()
     {
 
         return
-         "SELECT * FROM ticket WHERE ticket.author_id={$this->user->id}";
-
+            "SELECT * FROM ticket WHERE ticket.author_id={$this->user->id}";
     }
 }
