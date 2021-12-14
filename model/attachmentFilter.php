0c9e3fa4604a117f0b41872000ca95c450d76f26
ToukaGh <93223692+ToukaGh@users.noreply.github.com>
2021-12-14 22:54:39 +0200

    Update attachmentFilter.php

--- model/attachmentFilter.php
+++ model/attachmentFilter.php
@@ -4,14 +4,15 @@ require_once "filterDecorator.php";
 class AttachmentFilter extends FilterDecorator
 {
     public $ticket;
-    public function __construct($filter)
+    public function __construct($filter, $ticket)
     {
         $this->filter = $filter;
+        $this->ticket = $ticket;
     }
 
     public function generate()
     {
           return  $this->filter->generate(). " AND EXISTS (SELECT * FROM attachment WHERE attachment.ticket_id='{$this->ticket->id}')";
-          
+
     }
 }
