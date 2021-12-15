1061f9797d588c04f5ca8a5ff69a6a5bbbb7766a
maryam <93222257+maryamusama@users.noreply.github.com>
2021-12-15 22:55:00 +0200

    Update attatchment.php

--- model/attatchment.php
+++ model/attatchment.php
@@ -1,17 +1,17 @@
 <?php
-
-
+require_once 'ticket.php';
 class Attachment
 {
     public $id;
     public $url;
+    //public $ticket;
 
     public function __construct($id)
     {
         if (!$id) return;
 
         $stmt = DatabaseConnection::getInstance()->prepare(
-            "SELECT * FROM attachment WHERE Attachment_id=?"
+            "SELECT * FROM attachment WHERE attachment.id=?"
         );
 
         $stmt->bind_param('i', $id);
@@ -25,39 +25,9 @@ class Attachment
         $result = $stmt->get_result()->fetch_assoc();
 
         $this->id = $id;
-        $this->url = $result["URL"];
+        //$this->ticket = new Ticket($result["ticket_id"]);
+        $this->url = $result["url"];
     }
 
-    public function update()
-    {
-        $last_login = $this->lastLogin->format('Y-m-d H:i:s');
-        $stmt = DatabaseConnection::getInstance()->prepare(
-            "UPDATE attachment SET 
-                `URL`=?, "
-        );
-
-        $stmt->bind_param("s", $this->url);
-        return $stmt->execute();
-    }
-
-    public function delete()
-    {
-        $stmt = DatabaseConnection::getInstance()->prepare("DELETE FROM attachment WHERE  Attachment_id=?");
-        $stmt->bind_param("i", $this->id);
-        return $stmt->execute();
-    }
-
-    public static function create($url)
-    {
-        $type_id = 1;
-        $now = date_create()->format('Y-m-d H:i:s');
-        $stmt = DatabaseConnection::getInstance()->prepare(
-            "INSERT INTO attachment (
-                `URL`
-            ) VALUES (?)"
-        );
-
-        $stmt->bind_param('s', $url);
-        return $stmt->execute();
-    }
+   
 }
