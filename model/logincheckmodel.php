fed5428621436accfebef7a433a870a656ed4877
ToukaGh <93223692+ToukaGh@users.noreply.github.com>
2022-01-24 22:00:38 +0200

    Update logincheckmodel.php

--- model/logincheckmodel.php
+++ model/logincheckmodel.php
@@ -1,63 +1,72 @@
 <?php
-    include_once 'ExecQuery.php';
-    include_once 'ConcreteImp.php';
-  //  include_once 'usertype_pages.php';
-  //  require_once '../errorPage.php';
-    //include_once 'error_message_details.php';
-  //  require_once '../validation.php';
-  //  require_once 'type.php';
+   include_once 'ExecQuery.php';
+   include_once 'ConcreteImp.php';
+   require_once '../validation.php';
+   require_once '../model/type.php';
+   require_once "../model/database.php";
     class ProxyDP implements ExecQuery
     {
-        public $username;
+        
+        public $email;
         public $password;
-        public $userTypeObj;
-        function __construct($username,$password,$type)
+        public $con;
+        public $usertype;
+        public $Object;
+        function __construct($user)
         {
-            $con = mysqli_connect("localhost","root","","phase2");
-           
-            if(!$con)
-            {
-                die('Could not connect:' );
-             
-            }
-            
-            else
-            {
-                $sql="SELECT * FROM user WHERE email = '$username' AND password = '$password'";
-                $AdminDataSet = mysqli_query($con,$sql);
-                $num = mysqli_num_rows($AdminDataSet);
+             $con = DatabaseConnection::getInstance();
+            $email=$user->email;
+             $password=$user->password;
+                $stmt = DatabaseConnection::getInstance()->prepare(
+               "SELECT * FROM user
+               WHERE user.email=? AND user.Password=?"
+               );
+                
+                 $stmt->bind_param('ss', $email, $password);
+                $result = $stmt->execute();
+                $result = $stmt->get_result();
+                
+                 $row=$result->fetch_assoc();
+               // $AdminDataSet = $con->prepare($sql);
+                //$AdminDataSet->bind_param('ss', $email, $password);
+                //$AdminDataSet->execute();
+              
+                $num = mysqli_num_rows($result);
                 if($num > 0)
                 {
-                        $row = mysqli_fetch_array($AdminDataSet);
-                        $this->username = $row["email"];
+                    
+                        $this->email = $row["email"];
                         $this->password = $row["password"];
-                      //  $this->userTypeObj = new type($row["user_type_id"]);
-                        $Temp = $this->userTypeObj->id;
-                        $sql2 = "SELECT * FROM user_type";
-                        $UserTypeDataSet = mysqli_query($con,$sql2);
-                        while($row = mysqli_fetch_array($UserTypeDataSet))
-                        {
-                            if(  $this->username == $username && $this->password == $password)
-                            {
-                               // $sql3 = "SELECT * FROM user_type_pages WHERE usertype_id = '$Temp'";
+                        $this->Object = $row["user_type_id"];
+                          $stmt = DatabaseConnection::getInstance()->prepare(
+                                  
+                          "SELECT * FROM user_type WHERE id=?");
+                             $stmt->bind_param('i', $this->Object);
+                             $result = $stmt->execute();
+                             $result = $stmt->get_result();
+                             $row2=$result->fetch_assoc();
+                           if ($row2["name"] == "User") {
+                                 echo("user");
+                              include("view/viewall.php");
+                                 }
+                           if ($row2["name"] == "DepartmentHead") {
+                             include("view/viewall.php");
+                          }
+                       if ($row2["name"] == "Dispatcher") {
+                          include("view/viewall.php");
+                           }
+                               
+                               // $sql3 = "SELECT * FROM user_type_pages WHERE usertype_id = '$this->Object->name'";
                                 //$result = mysqli_query($con,$sql3);
                                 //$row2 = mysqli_fetch_array($result);
-                                echo("gg");
-                              //  $user_pageObj = new usertype_pages($row2["id"]);
+                                echo("usertype");
+                              //  $user_pageObj = new user_type_pages($row2["id"]);
                               //  if ($user_pageObj->page_id != "")
                                 //{
-                                  //  echo "<br><a href=".$user_pageObj->page_id->HTML."></a>";
-                                    break;
-                                //}
-                               
+                                  //  echo "<br><a href=".$Object->page_id->HTML."></a>";
+                                  
                             }
-        
-                        }
-                        /*if($ct10 == 0)
-                        {
-                            echo "Error type";
-                        }*/
-                }
+                        
                 else
                 {
                     $errs = new ErrorPage();
@@ -73,11 +82,8 @@
                 }
                 
            }
-           
-        }
-
         public function executeQuery($Query) {
-            echo("gg");
+            
             if($this->userTypeObj->name == "User")
             {
                 $admin = new ConcreteImp();
@@ -95,9 +101,6 @@
             }
     }
 }
-//$x = new ProxyDP("Kiro2003", "7bdfae2a3b7932a1547c231485dbe0d2a1c226e3", "Head of Servants");
-//echo $x->username . "<br>";
-//echo $x->password . "<br>";
-//echo $x->userTypeObj->name . "<br>";
-//Working !!!
+
+
 ?>
